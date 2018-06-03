988b7d1341bc52ae66d33f88da4ff1d7c266ea21
chi <nguyenlinhchi09@gmail.com>
2018-06-03 20:31:49 +0200

    plot interval

--- main.rkt
+++ main.rkt
@@ -1,7 +1,6 @@
 #lang racket
 (require "auto.rkt" "inout.rkt" "cons.rkt")
-(require plot racket/hash)
-(plot-new-window? #t)
+(require racket/hash)
 ;;(require racket/future)
 (provide (all-defined-out))
 ;;todo
@@ -86,46 +85,6 @@
      (hash-update h (flatten-automaton au) add1 0))
    (hash)
    p))
-
-(define (population-mean->lines data)
-  (define coors
-    (for/list ([d (in-list data)]
-               [n (in-naturals)])
-      (list n d)))
-  (lines coors))
-
-(define (compound d r)
-  (foldl (lambda (n a) (+ a (expt d n))) 1 (build-list (- r 1) add1)))
-
-(define (plot-mean-p data delta rounds)
-  (define low (* 2 (compound delta rounds)))
-  (define medium (* 5 (compound delta rounds)))
-  (define high (* 8 (compound delta rounds)))
-  (define line1
-    (function (lambda (x) low) #:color "blue"))
-  (define line2
-    (function (lambda (x) medium) #:color "green"))
-  (define line3
-    (function (lambda (x) high) #:color "red"))
-  (plot (list line1 line2 line3
-              (population-mean->lines data))
-        #:y-min 0 #:y-max (+ 5 high) #:width 1200 #:height 800))
-
-(define (plot-mean data delta rounds pic tit)
-  (define low (* 2 (compound delta rounds)))
-  (define medium (* 5 (compound delta rounds)))
-  (define high (* 8 (compound delta rounds)))
-  (define line1
-    (function (lambda (x) low) #:color "blue"))
-  (define line2
-    (function (lambda (x) medium) #:color "green"))
-  (define line3
-    (function (lambda (x) high) #:color "red"))
-  (plot (list line1 line2 line3
-              (population-mean->lines data))
-        #:y-min 0 #:y-max (+ 5 high) #:width 1200 #:height 800
-        #:out-file pic #:title tit))
-
 
 (define (sort-population p)
  (sort (hash->list (scan (vector-map reset p)))
